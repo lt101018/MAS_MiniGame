f922d5ca9a6e8eaafc545c7ad6df494aec250fb4
Teng Li <tengli@vt.edu>
2019-01-25 19:23:05 -0500

    fix gridlayout error

--- app/src/main/java/tengli/minigame/MainActivity.java
+++ app/src/main/java/tengli/minigame/MainActivity.java
@@ -2,7 +2,7 @@ package tengli.minigame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.widget.GridLayout;
 public class MainActivity extends AppCompatActivity {
 
     @Override
