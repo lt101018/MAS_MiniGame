f54918db698908e06bf629d2c455ff4ad1026eed
Teng Li <tengli@vt.edu>
2019-01-25 19:42:10 -0500

    fix same grid can put chess multiple times

--- app/src/main/java/tengli/minigame/MainActivity.java
+++ app/src/main/java/tengli/minigame/MainActivity.java
@@ -7,12 +7,24 @@ import android.widget.GridLayout;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
-
+    int currentPlayer = 0;
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     public void moveChess(View view){
         ImageView curGrid = (ImageView) view;
-        curGrid.setTranslationY(-1000f);
-        curGrid.setImageResource(R.drawable.lt);
-        curGrid.animate().translationYBy(1000f).rotation(360).setDuration(1000);
+        int gridCounter = Integer.parseInt(curGrid.getTag().toString());
+        if(gameState[gridCounter] == 2) {
+            if (currentPlayer == 0) {
+                curGrid.setTranslationY(-1000f);
+                curGrid.setImageResource(R.drawable.lt);
+                currentPlayer = 1;
+                curGrid.animate().translationYBy(1000f).rotation(360).setDuration(1000);
+            } else {
+                curGrid.setTranslationY(1000f);
+                curGrid.setImageResource(R.drawable.ly);
+                currentPlayer = 0;
+                curGrid.animate().translationYBy(-1000f).rotation(360).setDuration(1000);
+            }
+        }
     }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
