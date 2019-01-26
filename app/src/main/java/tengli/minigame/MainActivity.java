b55087646b21474b3113008213740032fe50cc7b
Teng Li <tengli@vt.edu>
2019-01-25 19:34:42 -0500

    add movechess method to move chess

--- app/src/main/java/tengli/minigame/MainActivity.java
+++ app/src/main/java/tengli/minigame/MainActivity.java
@@ -2,9 +2,18 @@ package tengli.minigame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.GridLayout;
+import android.widget.ImageView;
+
 public class MainActivity extends AppCompatActivity {
 
+    public void moveChess(View view){
+        ImageView curGrid = (ImageView) view;
+        curGrid.setTranslationY(-1000f);
+        curGrid.setImageResource(R.drawable.lt);
+        curGrid.animate().translationYBy(1000f).rotation(360).setDuration(1000);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
